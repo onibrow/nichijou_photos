66080ac7607469eb63dcfc8779e523980945cd03
onibrow <seiya.ono12@gmail.com>
2017-05-18 23:54:28 -0700

    Add a sleep timer

--- snap.sh
+++ snap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 3s
 streamer -f jpeg -o image.jpeg
 
 DICT="$(twurl -H upload.twitter.com '/1.1/media/upload.json' -f image.jpeg -F media -X POST)"
@@ -8,4 +9,5 @@ MEDIA_ID="$(python dict_parse.py ${DICT})"
 echo "${MEDIA_ID}"
 
 DATE="$(date)"
+
 twurl '/1.1/statuses/update.json' -d "media_ids=$MEDIA_ID&status=$DATE"
